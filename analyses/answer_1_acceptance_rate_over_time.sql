f7ae2a3becdd97390c56177e4e4b72a9f848827a
Gean Pannellini <geanpanne@gmail.com>
2024-02-08 04:43:04 +0000

    fix: answer_1

--- analyses/answer_1_acceptance_rate_over_time.sql
+++ analyses/answer_1_acceptance_rate_over_time.sql
@@ -1,5 +1,5 @@
 
-SELECT  {{ define_semiannually('date_time') }} AS monthly,
+SELECT  {{ define_quarterly('date_time') }} AS monthly,
         state,
         count(state) AS count,
         COUNT(*) * 100.0 / (SELECT COUNT(*) FROM {{ ref('gold__transactions') }}) AS percentage
